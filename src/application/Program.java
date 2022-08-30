fc38dbd35008ef0083dc3dc48a5e7f94b682444b
celiollima <celiollimaner@gmail.com>
2022-08-30 13:40:32 -0300

    position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 
-		System.out.println("Olá play Xadrez!");
-
+		Position position = new Position(3, 5);
+		System.out.println(position);
 	}
 
 }
